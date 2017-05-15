21d5ea480da6c5e2b6f32ab9d66ba20aae75ca79
design <design@POMPEII13.SEDC.local>
2017-05-15 20:52:52 +0200

    including php files

---
+++ class_6/about.php
@@ -0,0 +1,9 @@
+<?php
+include 'functions.php';
+$title = "SEDC";
+?>
+<?php include 'includes/header.php'; ?>
+<?php var_dump($_SERVER); ?>
+        <h3><?php echo hello(); ?></h3>
+<?php include 'includes/footer.php'; ?>
+
---
+++ class_6/index.php
@@ -0,0 +1,9 @@
+<?php
+ini_set('display_errors', true);
+require 'functions.php';
+$title = "SEDC";
+?>
+<?php include 'includes/header.php'; ?>
+<?php// var_dump($_SERVER); ?>
+        <h1><?php echo hello(); ?></h1>
+<?php include 'includes/footer.php'; ?>
