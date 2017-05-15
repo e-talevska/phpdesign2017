2cc9cb8942efd73a41f191112960ac038f84385c
design <design@POMPEII05.SEDC.local>
2017-05-15 20:53:02 +0200

    Including PHP files

---
+++ cas6/about.php
@@ -0,0 +1,8 @@
+<?php
+include 'functions.php';
+$title = "about";
+?>
+<?php include 'includes/header.php'; ?>
+<?php var_dump ($_SERVER); ?>
+        <h1><?php echo hello() ?></h1>
+<?php include 'includes/footer.php'; ?>
---
+++ cas6/index.php
@@ -0,0 +1,7 @@
+<?php
+include 'functions.php';
+$title = "SEDC";
+?>
+<?php include 'includes/header.php'; ?>
+        <h1><?php echo hello() ?></h1>
+<?php include 'includes/footer.php'; ?>
