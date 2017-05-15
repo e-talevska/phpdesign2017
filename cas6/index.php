c11a8afb6029b45871d3c2ed899edd16c99bb7a4
design <design@POMPEII16.SEDC.local>
2017-05-15 20:52:50 +0200

    Including Php files

---
+++ cas6/index.php
@@ -0,0 +1,10 @@
+<?php 
+include 'functions.php';
+$title ="SEDC";
+?>
+
+<?php include 'includes/header.php'; ?>
+<?php var_dump($_SERVER); ?>
+        <h1><?php echo hello(); ?></h1>
+<?php include 'includes/footer.php'; ?>
+  
