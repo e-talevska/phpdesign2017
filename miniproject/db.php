4d2b8562e523e64a37540995dd1636ed5a1140e1
emilija.talevska <emilija.talevska@POMPEII00.SEDC.local>
2017-05-17 21:03:42 +0200

    Using query parameters

---
+++ miniproject/db.php
@@ -0,0 +1,19 @@
+<?php
+
+$portfolios = [
+  [
+      'img' => 'http://placehold.it/700x450',
+      'header' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel ex lacinia, euismod diam sed, pellentesque tellus. Ut lorem felis, pharetra id dui in, lacinia fringilla arcu. Morbi faucibus nisi in lectus viverra imperdiet. In rhoncus bibendum auctor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer hendrerit urna ac arcu sodales, eget finibus est egestas. Cras imperdiet placerat augue, eget consequat ligula. In mollis aliquam fermentum.'
+  ],
+   [
+      'img' => 'http://placehold.it/700x450',
+      'header' => 'Duis luctus tortor sed nisl luctus tincidunt. ',
+      'text' => 'Duis luctus tortor sed nisl luctus tincidunt. Morbi sit amet erat quis eros euismod efficitur. Duis sagittis libero mauris. Maecenas feugiat sem erat. Mauris rhoncus suscipit purus, non luctus nisl pulvinar eu. Sed orci mi, euismod ut tortor et, scelerisque blandit neque. Donec sit amet fermentum orci, vitae commodo magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce dictum libero eget dignissim imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus ac mauris vel nibh interdum sodales. Vestibulum accumsan augue in elit cursus porta. Ut blandit nisi in justo tristique, vel consectetur ex fringilla. Sed varius pharetra semper. Proin nisi sem, suscipit porttitor semper ac, rhoncus eu magna.'
+  ],
+  [
+      'img' => 'http://placehold.it/700x450',
+      'header' => 'Sed id eleifend orci, sit amet pulvinar mauris.',
+      'text' => 'Sed id eleifend orci, sit amet pulvinar mauris. Ut tempor mauris at purus finibus, ut varius massa dignissim. Quisque faucibus ultrices odio, at accumsan magna pretium non. Proin a maximus orci. Morbi egestas mauris eu leo porta, ac maximus lacus tristique. Etiam convallis ipsum eu fringilla sodales. Maecenas ut est id mi ornare convallis ut facilisis diam. Quisque tristique eu felis gravida finibus. Proin id magna tortor. Sed sollicitudin mi ut nulla iaculis, non sodales ligula malesuada. Fusce viverra massa id risus imperdiet elementum. Praesent dui ligula, finibus sed facilisis ac, euismod sit amet quam. Sed ultrices purus ut mi mattis, at pellentesque tellus lacinia. Quisque faucibus quis libero sed porttitor. Morbi lorem metus, consectetur nec mauris ut, blandit vehicula diam.'
+  ]  
+];
