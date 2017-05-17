e234febb03c518405b799971b3844f3cee95163c
design <design@POMPEII09.SEDC.local>
2017-05-17 21:04:30 +0200

    Using query parameters

--- cas6/index.php
+++ cas6/index.php
@@ -10,6 +10,6 @@
 <?php include 'includes/header.php'; ?>
 <?php //var_dump($_SERVER); ?>
         <h1><?php echo hello() ?></h1>
-<?php include 'includes/footer.php'; ?>        
+<?php include 'includes/footer.php';     
 
 
