b87f06932f5156ec2eee9b04d127d5ea2d48ba0d
design <design@POMPEII13.SEDC.local>
2017-05-17 21:03:06 +0200

    Using Query Parameters

---
+++ mini_project/db.php
@@ -0,0 +1,36 @@
+<?php 
+
+$portfolios = [
+	[
+		'img' => 'http://placehold.it/700x450',
+		'header' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do',
+		'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	],
+	[
+		'img' => 'http://placehold.it/700x450',
+		'header' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do',
+		'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	],
+	[
+		'img' => 'http://placehold.it/700x450',
+		'header' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do',
+		'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	],
+];
+
+ ?>
