9933f39f27924b5eee0c6413055ad9f429b639ec
design <design@POMPEII01.SEDC.local>
2017-05-22 20:41:31 +0200

    working with forms & sending email

--- miniproject/contact.php
+++ miniproject/contact.php
@@ -1,8 +1,48 @@
-<?php include 'header.php' ?>
+<?php include 'header.php'; var_dump($_POST); ?>
+<?php 
+    //if the user submitted the form
+    $errorName =  $errorName02 = '';
+    $fullName = $phone = $email = $message = '';
+    if(isset($_POST['submit'])){
+        $fullName = $_POST['full_name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        // this is what trim() does "  gfdfgd " => "gfdfgd"       
+        if(strlen(trim($fullName)) == 0){
+            $errorName =  "Full name is required!";
+        }
+        if(strlen(trim($email)) == 0){
+            $errorName02 =  "Email is required!";
+        }else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errorEmail = "Email not valid.";
+        }
+        if(strlen(trim($message)) == 0){
+            $errorName03 =  "Message is required!";
+        }
+        //no errors in validation 
+        //send the mail
+        if(empty($errorName) && empty($errorName02) && empty($errorName03)){
+            require 'helper.php';
+            $emailMessage = "The user $fullName with email $email has sent you the following: <br />" . $message;
+            $mailSuccess = sendMail($email, $fullName, "Contact from webpage.", $emailMessage);
+            $fullName = $phone = $email = $message = '';
+        }
+
+    } 
+    //dvata if pravat edna ista proverka
+    // if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    // }
+
+?>
 
     <!-- Page Content -->
     <div class="container">
-
+    <?php if(isset($mailSuccess)){ 
+        $class = ($mailSuccess['error'] == true) ? 'alert-danger' : 'alert-success';
+        echo "<div class='alert $class'> {$mailSuccess['message']} </div>"; 
+        } ?>
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
@@ -61,35 +101,37 @@
         <div class="row">
             <div class="col-md-8">
                 <h3>Send us a Message</h3>
-                <form name="sentMessage" id="contactForm" novalidate>
+                <form method="post"  name="sentMessage" id="contactForm" novalidate>
                     <div class="control-group form-group">
                         <div class="controls">
                             <label>Full Name:</label>
-                            <input type="text" class="form-control" id="name" required data-validation-required-message="Please enter your name.">
-                            <p class="help-block"></p>
+                            <input type="text" class="form-control" id="name" required  name="full_name"data-validation-required-message="Please enter your name." value="<?php echo $fullName; ?>">
+                            <p class="help-block"><?php if(isset($errorName)){ echo $errorName; } ?></p>
                         </div>
                     </div>
                     <div class="control-group form-group">
                         <div class="controls">
                             <label>Phone Number:</label>
-                            <input type="tel" class="form-control" id="phone" required data-validation-required-message="Please enter your phone number.">
+                            <input type="tel" class="form-control" id="phone" required name="phone"data-validation-required-message="Please enter your phone number." value="<?php echo $phone; ?>"> 
                         </div>
                     </div>
                     <div class="control-group form-group">
                         <div class="controls">
                             <label>Email Address:</label>
-                            <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
+                            <input type="email" class="form-control" id="email" required name="email"data-validation-required-message="Please enter your email address." value="<?php echo $email; ?>">
+                            <p class="help-block"><?php if(isset($errorName02)){ echo $errorName02; }if(isset($errorEmail)){echo $errorEmail;} ?></p>
                         </div>
                     </div>
                     <div class="control-group form-group">
                         <div class="controls">
                             <label>Message:</label>
-                            <textarea rows="10" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
+                            <textarea rows="10" cols="100" class="form-control" id="message" required name="message"data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"><?php echo $message; ?></textarea >
+                            <p class="help-block"><?php if(isset($errorName03)){ echo $errorName03; } ?></p>
                         </div>
                     </div>
                     <div id="success"></div>
                     <!-- For success/fail messages -->
-                    <button type="submit" class="btn btn-primary">Send Message</button>
+                    <button type="submit" name="submit" class="btn btn-primary">Send Message</button>
                 </form>
             </div>
 
---
+++ miniproject/helper.php
@@ -0,0 +1,53 @@
+<?php 
+
+	function sendMail($fromEmail, $fromName, $subject, $message){
+		require 'PHPMailer/PHPMailerAutoload.php';
+
+		$mail = new PHPMailer;
+
+		//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+		$mail->isSMTP();                                      // Set mailer to use SMTP- 
+		$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+		$mail->SMTPAuth = true;                               // Enable SMTP authentication
+		$mail->Username = 'sedc.academy@gmail.com';                 // SMTP username
+		$mail->Password = 'sedc1234';                           // SMTP password
+		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+		$mail->Port = 587;                                    // TCP port to connect to
+
+		$mail->setFrom( $fromEmail, $fromName);
+		$mail->addAddress('sedc.academy@gmail.com', 'Design PHP & Mysql');     // Add a recipient
+		// $mail->addAddress('ellen@example.com');               // Name is optional
+		// $mail->addReplyTo('info@example.com', 'Information');
+		// $mail->addCC('cc@example.com');
+		// $mail->addBCC('bcc@example.com');
+
+		// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+		$mail->isHTML(true);                                  // Set email format to HTML
+
+		$mail->Subject = $subject;
+		$mail->Body    = $message;
+		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+		if(!$mail->send()) {
+		    return  [
+
+		    	'error' => true,
+		    	'message' => 'Message could not be sent.'
+
+		    ];
+		    // echo 'Mailer Error: ' . $mail->ErrorInfo;
+		} else {
+		    return  [
+
+		    	'error' => false,
+		    	'message' => 'Message has been sent.'
+
+		    ];
+		}
+			
+
+	}
+
+ ?>
