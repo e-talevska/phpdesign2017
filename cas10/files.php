991c5482af858c63d42cb53ea78a4619b20819b0
Design <design@SEDC.local>
2017-05-26 20:26:06 +0200

    Upload na sliki

---
+++ cas10/files.php
@@ -0,0 +1,31 @@
+<?php
+$filename = 'text.txt';
+$handler = fopen($filename, 'r');
+// $content = fread($handler, filesize($filename)); //dolzinata e vo bytes
+$content = fread($handler, 3);
+$content .= fread($handler, 3);
+fclose($handler);
+
+echo "Content:" . $content . "</br>";
+
+//substitute for the lines above
+$whole_content = file_get_contents($filename);
+echo "Content:" . $whole_content . "</br>";
+
+$handler = fopen($filename, 'r');
+while(!feof($handler)){
+    //$line = gets($handler);
+    // $line = str_replace(PHP_EOL, '', fgets($handler));
+     $values = fgetcsv($handler);
+    var_dump($values);
+}
+fclose($handler);
+
+$handler = fopen($filename, 'a');
+fwrite($handler, "sedc,php".PHP_EOL);
+fclose($handler);
+
+echo file_get_contents($filename);
+
+
+
--- miniproject/admin.php
+++ miniproject/admin.php
@@ -2,7 +2,40 @@
     require 'helper.php';
     checkLoggedIn();
 ?>
+<?php
+$errortitle = isset($_SESSION['error_title']) ? $_SESSION['error_title'] : '';
+$errortext = isset($_SESSION['error_text']) ? $_SESSION['error_text'] : '';
+$errorImage = isset($_SESSION['error_image']) ? $_SESSION['error_image'] : '';
+$title = isset($_SESSION['title']) ? $_SESSION['title'] : '';
+$text = isset($_SESSION['text']) ? $_SESSION['text']: '';
+unset($_SESSION['error_title'],
+        $_SESSION['error_text'],
+        $_SESSION['error_image'],
+        $_SESSION['title'],
+        $_SESSION['text']);
+?>
 <?php include 'templates/header.php'; ?>
 <div class="container"> 
     <h1 class="page-header">Admin Page</h1>
+
+
+<form method="POST" enctype="multipart/form-data" action="add_portfolio.php">
+            <div class="form-group">
+              <label for="title">Title</label>
+              <input name="title" value="<?php echo $title; ?>" type="text" class="form-control" id="title" placeholder="title">
+              <p class="help-block"><?php echo $errortitle; ?></p>
+            </div>
+            <div class="form-group">
+              <label for="text">Content</label>
+              <textarea rows="3" name="text" class="form-control" id="textarea"><?php echo $text; ?></textarea>
+               <p class="help-block"><?php echo $errortext; ?></p>
+            </div>
+            <div class="help-block">
+                <label for="image">Image</label>
+                <input name="image" type="file" class="form-control" id="image"> 
+                <p class="help-block"><?php echo $errorImage; ?></p>
+            </div>
+            <button name="submit" type="submit" class="btn btn-default">Submit</button>
+          </form>
 <?php include 'templates/footer.php'; ?>
+
