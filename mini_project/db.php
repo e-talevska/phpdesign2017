b53bbdca43e35ee287557e1897c6db4a0ec53a34
design <design@POMPEII03.SEDC.local>
2017-05-26 20:27:17 +0200

    working with files

--- mini_project/db.php
+++ mini_project/db.php
@@ -1,18 +1,19 @@
 <?php
 	$portfolios = [
 		
-			['img' => 'http://placehold.it/700x450',
-			'header' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit, ',
-			'text' => 'consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'],
+			[	
+				'img' => 'http://placehold.it/700x450',
+				'header' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit, ',
+				'text' => 'consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+			],
 
-			['img' => 'http://placehold.it/700x450',
-						'header' => 'Duis aute irure dolor in reprehenderit in voluptate ',
-						'text' => 'Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur.'],
+			[	'img' => 'http://placehold.it/700x450',
+				'header' => 'Duis aute irure dolor in reprehenderit in voluptate ',
+				'text' => 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'],
 
-			['img' => 'http://placehold.it/700x450',
-						'header' => 'Excepteur sint occaecat cupidatat non',
-						'text' => ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
+			[	'img' => 'http://placehold.it/700x450',
+				'header' => 'Excepteur sint occaecat cupidatat non',
+				'text' => ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim i laborum.']
 		
 
 	];
